7d41955c6f65962ed983c591ddaf6f51622e342c
AYogore <AYogore@users.noreply.github.com>
2020-10-10 12:58:56 -0500

    GameFlow enabled
    platform spawn intervals working

---
+++ KaijuKiller/Assets/gameFlow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameFlow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform tileObj;
+    private Vector3 nextTileSpawn;
+    [SerializeField]
+    private int platformLength;
+    void Start()
+    {
+        nextTileSpawn.z = platformLength;
+        StartCoroutine(spawnTile());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator spawnTile()
+    {
+        yield return new WaitForSeconds(1);
+        Instantiate(tileObj, nextTileSpawn, tileObj.rotation);
+        nextTileSpawn.z += platformLength;
+        StartCoroutine(spawnTile());
+
+        /* destory object after 5 seconds
+        yield return new WaitForSeconds(5);
+        Destroy(tileObj);
+        */
+    }
+}
