4c69f1246ad41b06fa0985a71129b2d9d8476e52
AYogore <AYogore@users.noreply.github.com>
2020-11-03 21:06:25 -0600

    PewPew working

---
+++ KaijuKiller/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bulletPrefab;
+
+    [SerializeField]
+    private Vector3 firepoint;
+
+    
+    private int fireRate = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //Instantiate(bulletPrefab, firepoint, bulletPrefab.transform.rotation);
+            Debug.Log("Pew");
+        }
+    }
+
+    IEnumerator Fire()
+    {
+        yield return new WaitForSeconds(1 / fireRate);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //Instantiate(bulletPrefab, firepoint, bulletPrefab.transform.rotation);
+            Debug.Log("Pew");
+        }
+    }
+}
