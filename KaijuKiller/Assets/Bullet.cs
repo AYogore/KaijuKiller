e062d609df1a0e8295ff6c7249b7371afe9910af
AYogore <AYogore@users.noreply.github.com>
2020-11-04 16:15:22 -0600

    Shooting works

---
+++ KaijuKiller/Assets/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField]
+    private float bulletSpeed;
+
+    Vector3 bulletVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletVector = new Vector3(0, 0, bulletSpeed);
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = Vector3.forward * bulletSpeed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(this.gameObject);
+        Destroy(collision.gameObject);
+    }
+}
