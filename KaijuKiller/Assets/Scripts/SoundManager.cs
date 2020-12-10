1948d2bdda750caee36d7b234957197d7cf97636
AYogore <AYogore@users.noreply.github.com>
2020-12-09 23:56:32 -0600

    Sound Manager and added sounds

---
+++ KaijuKiller/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource deathSound;
+    [SerializeField]
+    private AudioSource coinSound;
+    [SerializeField]
+    private AudioSource boostSound;
+    
+    public void PlayDeath()
+    {
+        deathSound.Play();
+    }
+    public void PlayCoin()
+    {
+        coinSound.Play();
+    }
+    public void PlayBoost()
+    {
+        boostSound.Play();
+    }
+}
