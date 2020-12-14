f53e6ac7984c5fd1bd31197df239b64fb0cb5a65
AYogore <AYogore@users.noreply.github.com>
2020-12-13 22:53:51 -0600

    Added Menu Panels and UI

--- KaijuKiller/Assets/Scripts/ObjectCollision.cs
+++ KaijuKiller/Assets/Scripts/ObjectCollision.cs
@@ -12,8 +12,13 @@ public class ObjectCollision : MonoBehaviour
 
     [SerializeField]
     private Button restartButton;
+
+    [SerializeField]
+    private Button quitButton; 
+
     CanvasGroup restartButtonCanvasGroup;
     CanvasGroup deathScreenCanvasGroup;
+    CanvasGroup quitButtonCanvasGroup;
 
     [SerializeField]
     TextMeshProUGUI scoreText;
@@ -24,10 +29,14 @@ public class ObjectCollision : MonoBehaviour
     {
         deathScreenCanvasGroup = deathScreen.GetComponent<CanvasGroup>();
         restartButtonCanvasGroup = restartButton.GetComponent<CanvasGroup>();
+        quitButtonCanvasGroup = quitButton.GetComponent<CanvasGroup>();
 
+
+        quitButton.interactable = false;
         restartButton.interactable = false;
         deathScreenCanvasGroup.alpha = 0;
         restartButtonCanvasGroup.alpha = 0;
+        quitButtonCanvasGroup.alpha = 0;
 
     }
 
@@ -44,6 +53,8 @@ public class ObjectCollision : MonoBehaviour
             Time.timeScale = 0;
             deathScreenCanvasGroup.alpha = 1;
             restartButtonCanvasGroup.alpha = 1;
+            quitButtonCanvasGroup.alpha = 1;
+            quitButton.interactable = true;
             restartButton.interactable = true;
 
         }
